1d261f022f87ec9d443eb95eadc1bea6ea0c4a0e
just-toni <75427368+just-toni@users.noreply.github.com>
2023-05-04 23:51:28 -0300

    Updated games.js to add playRound function

--- game.js
+++ game.js
@@ -11,3 +11,34 @@ function getComputerChoice(){
     }
 }
 
+function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase()
+    computerSelection = computerSelection.toLowerCase()
+    if(playerSelection === 'rock' && computerSelection === 'rock'){
+        return `It's a tie`
+    }
+    else if(playerSelection === 'rock' && computerSelection === 'paper'){
+        return `You win! Rock beats Paper`
+    }
+    else if (playerSelection === 'paper' && computerSelection === 'rock'){
+        return `You lose! Rock beats Paper`
+    }
+    else if(playerSelection === 'scissors' && computerSelection === 'paper'){
+        return `You win! Scissors beats Paper`
+    }
+    else if (playerSelection === 'paper' && computerSelection === 'scissors'){
+        return `You lose! Scissors beats Paper`
+    }    
+    else if(playerSelection === 'rock' && computerSelection === 'scissors'){
+        return `You win! Rock beats Scissors`
+    }
+    else if (playerSelection === 'scissors' && computerSelection === 'rock'){
+        return `You lose! Rock beats Scissors`
+    }
+    else if (playerSelection === 'paper' && computerSelection === 'paper'){
+        return `It's a tie`
+    }
+    else if (playerSelection === 'scissors' && computerSelection === 'scissors'){
+        return `It's a tie`
+    }
+}
