58ccd903ce78b0c06b51ca3e793fcceb4689b334
just-toni <75427368+just-toni@users.noreply.github.com>
2023-05-24 00:52:17 -0300

    created new function called playRound and ensures it works

--- game.js
+++ game.js
@@ -1,7 +1,35 @@
 let options = ['Rock', 'Paper', 'Scissors']
 function getComputerChoice(){
     let index = Math.floor(Math.random() * 3);
-    console.log(options[index]);
+    return (options[index].toLowerCase());
 };
 
-getComputerChoice();
+function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase();
+    if(computerSelection === playerSelection){
+        return 'It is a tie.';
+    }
+    if(playerSelection === 'rock' && computerSelection === 'paper'){
+        return 'You lose! Paper beats Rock';
+    }
+    if(playerSelection === 'paper' && computerSelection === 'rock'){
+        return 'You win! Paper beats Rock';
+    }
+    if(playerSelection === 'rock' && computerSelection === 'scissors'){
+        return 'You win! Rock beats Scissors';
+    }
+    if(playerSelection === 'scissors' && computerSelection === 'rock'){
+        return 'You lose! Rock beats Scissors';
+    }
+    if(playerSelection === 'paper' && computerSelection === 'scissors'){
+        return 'You lose! Scissors beats Paper';
+    }
+    if(playerSelection === 'scisspors' && computerSelection === 'paper'){
+        return 'You win! Scissors beats Paper';
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection));
