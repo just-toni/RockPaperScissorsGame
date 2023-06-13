8ab482d3fc609e3ab685f2a7770c618f352d7420
just-toni <75427368+just-toni@users.noreply.github.com>
2023-06-13 18:09:15 -0300

    made progress with the bug but still fixing up some things, there's currently no design or style

--- game.js
+++ game.js
@@ -6,35 +6,58 @@ function getComputerChoice(){
 
 let result, playerScore = 0, computerScore = 0;
 
+function displayResult(){
+    let div = document.createElement('div');
+    let p = document.createElement('p');
+    if(playerScore > computerScore){
+        p.textContent = `Computer scores ${computerScore - 1} \n You score ${playerScore - 1} \n You win!`;
+        div.appendChild(p);
+        body.appendChild(div);
+    }
+    else if(playerScore === computerScore){
+        p.textContent = `Computer scores ${computerScore - 1} \n You score ${playerScore - 1} \n It's a tie!`;
+        div.appendChild(p);
+        body.appendChild(div);
+    }
+    else{
+        p.textContent = `Computer scores ${computerScore - 1} \n You score ${playerScore - 1} \n Computer wins!`;
+        div.appendChild(p);
+        body.appendChild(div);
+    }
+}
+
 function playRound(playerSelection, computerSelection){
     playerSelection = playerSelection.toLowerCase();
-    if(computerSelection === playerSelection){
-        return 'It is a tie.';
+    while(playerScore !== 6){
+        if(computerSelection === playerSelection){
+            return 'It is a tie.';
+        }
+        if(playerSelection === 'rock' && computerSelection === 'paper'){
+            ++computerScore;
+            return 'You lose! Paper beats Rock';
+        }
+        if(playerSelection === 'paper' && computerSelection === 'rock'){
+            ++playerScore;
+            return `You win! Paper beats Rock. Your score is ${playerScore}`;
+        }
+        if(playerSelection === 'rock' && computerSelection === 'scissors'){
+            ++playerScore;
+            return `You win! Rock beats Scissors. Your score is ${playerScore}`;
+        }
+        if(playerSelection === 'scissors' && computerSelection === 'rock'){
+            ++computerScore;
+            return 'You lose! Rock beats Scissors';
+        }
+        if(playerSelection === 'paper' && computerSelection === 'scissors'){
+            ++computerScore;
+            return 'You lose! Scissors beats Paper';
+        }
+        if(playerSelection === 'scissors' && computerSelection === 'paper'){
+            ++playerScore;
+            return `You win! Scissors beats Paper. Your score is ${playerScore}`;
+        }
     }
-    if(playerSelection === 'rock' && computerSelection === 'paper'){
-        computerScore++;
-        return 'You lose! Paper beats Rock';
-    }
-    if(playerSelection === 'paper' && computerSelection === 'rock'){
-        playerScore++;
-        return `You win! Paper beats Rock. Your score is ${playerScore}`;
-    }
-    if(playerSelection === 'rock' && computerSelection === 'scissors'){
-        playerScore++;
-        return `You win! Rock beats Scissors. Your score is ${playerScore}`;
-    }
-    if(playerSelection === 'scissors' && computerSelection === 'rock'){
-        computerScore++;
-        return 'You lose! Rock beats Scissors';
-    }
-    if(playerSelection === 'paper' && computerSelection === 'scissors'){
-        computerScore++;
-        return 'You lose! Scissors beats Paper';
-    }
-    if(playerSelection === 'scissors' && computerSelection === 'paper'){
-        playerScore++;
-        return `You win! Scissors beats Paper. Your score is ${playerScore}`;
-    }
+    displayResult();
 }
 
 let body = document.querySelector('body');
@@ -50,47 +73,51 @@ body.appendChild(rock);
 body.appendChild(scissors);
 body.appendChild(paper);
 
-if(playerScore != 6){
-    rock.addEventListener('click', e=>{
-        let div = document.createElement('div');
-        let p = document.createElement('p');
-        p.textContent = playRound('rock', getComputerChoice());
+
+/**
+ * problems
+ * how to display the p1 text once and it would just be updated and not repeated or appended
+ * how to stop the entire game once the player score is 5 and display Results
+ */
+
+rock.addEventListener('click', e=>{
+    let div = document.createElement('div');
+    let p = document.createElement('p');
+    p.textContent = playRound('rock', getComputerChoice());
+    if(playerScore <= 5){
         let p1 = document.createElement('p');
-        p1.textContent = 'This round: ' + p.textContent;
+        p1.textContent = 'Rock round: ' + p.textContent;
         div.appendChild(p1);    
-        body.appendChild(div);
-    });
-    scissors.addEventListener('click', e=>{
-        let div = document.createElement('div');
-        let p = document.createElement('p');
-        p.textContent = playRound('scissors', getComputerChoice());
-        let p1 = document.createElement('p');
-        p1.textContent = 'This round: ' + p.textContent;
-        div.appendChild(p1);    
-        body.appendChild(div);
-    });
-    paper.addEventListener('click', e=>{
-        let div = document.createElement('div');
-        let p = document.createElement('p');
-        p.textContent = playRound('paper', getComputerChoice());
-        let p1 = document.createElement('p');
-        p1.textContent = 'This round: ' + p.textContent;
-        div.appendChild(p1);    
-        body.appendChild(div);
-    })
-}
-else{
-    if(playerScore > computerScore){
-        p.textContent = `Computer scores ${computerScore} \n You score ${playerScore} \n You win!`;
-        div.appendChild(p);
         body.appendChild(div);
     }
-    else{
-        p.textContent = `Computer scores ${computerScore} \n You score ${playerScore} \n Computer wins!`;
-        div.appendChild(p);
+ 
+});
+scissors.addEventListener('click', e=>{
+    let div = document.createElement('div');
+    let p = document.createElement('p');
+    p.textContent = playRound('scissors', getComputerChoice());
+    if(playerScore <= 5){
+        let p1 = document.createElement('p');
+        p1.textContent = 'Scissors round: ' + p.textContent;
+        div.appendChild(p1);    
         body.appendChild(div);
     }
-}
+
+});
+paper.addEventListener('click', e=>{
+    let div = document.createElement('div');
+    let p = document.createElement('p');
+    p.textContent = playRound('paper', getComputerChoice());
+    if(playerScore <= 5){
+        let p1 = document.createElement('p');
+        p1.textContent = 'Paper round: ' + p.textContent;
+        div.appendChild(p1);    
+        body.appendChild(div);
+    }
+
+});
+
+
 
 // rock.addEventListener('click', e=>{
 //     let div = document.createElement('div');
